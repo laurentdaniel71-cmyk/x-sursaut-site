6b8b04a41d57eed9f82c1301c247fbf8f8fb4b4c
laurentdaniel71-cmyk <laurentdaniel71@gmail.com>
2025-08-24 18:54:14 +0200

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Bienvenue sur X-Sursaut 🚀</h1>
+      <p>Ceci est la page d’accueil de notre think tank.</p>
+    </main>
+  )
+}
